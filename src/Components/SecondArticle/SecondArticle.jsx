ed782d015c1d2acd3ca957352648a8db53df049e
arthurbochicruz <173960792+arthurbochicruz@users.noreply.github.com>
2025-07-02 18:24:51 -0300

    Commit do  segundo artigo
    Encerrei a estilização do SecondArticle

--- src/Components/SecondArticle/SecondArticle.jsx
+++ src/Components/SecondArticle/SecondArticle.jsx
@@ -3,7 +3,21 @@ import './SecondArticle.css'
 function SecondArticle() {
     return (
         <section>
-            
+            <div className='div-titles'>
+                <h2 className='title-atendimentos'>
+                Setorize o seu atendimento com <br /> apenas 1 Número de<br />WhatsApp.
+                </h2>
+
+                <p className='par-Chat'>
+                    A Souchat permite que a sua empresa tenha <br /> diversos setores de atendimento com apenas <strong>1 <br /> número de WhatsApp</strong> e com distribuição de forma <br /> automática das mensagens dos clientes entre <br /> esses usuários e setores.
+                </p>
+
+                <div className='div-a-button'>
+                    <a className='a-plans' href="#">
+                        <span>Conheça Nossos Planos</span>
+                    </a>
+                </div>
+            </div>
         </section>
     )
 }
