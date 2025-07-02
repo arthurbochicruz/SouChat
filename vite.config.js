93de1ab2b8acdc320d518c1f4cef65a3cb9f169a
arthurbochicruz <173960792+arthurbochicruz@users.noreply.github.com>
2025-07-02 17:16:20 -0300

    Commit json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+})*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/SouChat/', // <- exatamente o nome do seu repositório
+  plugins: [react()],
 })
+
